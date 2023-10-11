a59c48ddde2f5d803adb1f6becb0f683f804cc75
Wesley Silva <wesley.sousa59@gmail.com>
2023-10-11 08:37:07 -0300

    initial commit

---
+++ src/main/java/br/com/wss/todolist/user/UserModel.java
@@ -0,0 +1,14 @@
+package br.com.wss.todolist.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+}
